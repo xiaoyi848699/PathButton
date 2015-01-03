07a41a08edaa0f8d33e2bcbcb5dadc916075208e
xiaoyi <xiaoyi@xiaoyi-PC>
2015-01-03 17:07:42 +0800

    finish first pathButton

--- src/com/example/pathbutton/MainActivity.java
+++ src/com/example/pathbutton/MainActivity.java
@@ -1,22 +1,183 @@
 package com.example.pathbutton;
 
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+	private View mUgcView;
+	private RelativeLayout mUgcLayout;
+	private ImageView mUgc;
+	private ImageView mUgcBg;
+	private ImageView mUgcVoice;
+	private ImageView mUgcPhoto;
+	private ImageView mUgcRecord;
+	private ImageView mUgcLbs;
+	/**
+	 * 判断当前的path菜单是否已经显示
+	 */
+	private boolean mUgcIsShowing = false;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		initView();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private void initView() {
+		mUgcView = (View) findViewById(R.id.home_ugc);
+		mUgcLayout = (RelativeLayout) findViewById(R.id.ugc_layout);
+		mUgc = (ImageView) findViewById(R.id.ugc);
+		mUgcBg = (ImageView) findViewById(R.id.ugc_bg);
+		mUgcVoice = (ImageView) findViewById(R.id.ugc_voice);
+		mUgcPhoto = (ImageView) findViewById(R.id.ugc_photo);
+		mUgcRecord = (ImageView) findViewById(R.id.ugc_record);
+		mUgcLbs = (ImageView) findViewById(R.id.ugc_lbs);
+		// Path监听
+		mUgcView.setOnTouchListener(new OnTouchListener() {
+
+			public boolean onTouch(View v, MotionEvent event) {
+				// 判断是否已经显示,显示则关闭并隐藏
+				if (mUgcIsShowing) {
+					mUgcIsShowing = false;
+					UgcAnimations.startCloseAnimation(mUgcLayout, mUgcBg, mUgc,
+							500);
+					return true;
+				}
+				return false;
+			}
+		});
+		// Path监听
+		mUgc.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				// 判断是否显示,已经显示则隐藏,否则则显示
+				mUgcIsShowing = !mUgcIsShowing;
+				if (mUgcIsShowing) {
+					UgcAnimations.startOpenAnimation(mUgcLayout, mUgcBg, mUgc,
+							500);
+				} else {
+					UgcAnimations.startCloseAnimation(mUgcLayout, mUgcBg, mUgc,
+							500);
+				}
+			}
+		});
+		// Path 语音按钮监听
+		mUgcVoice.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				Animation anim = UgcAnimations.clickAnimation(500);
+				anim.setAnimationListener(new AnimationListener() {
+
+					public void onAnimationStart(Animation animation) {
+
+					}
+
+					public void onAnimationRepeat(Animation animation) {
+
+					}
+
+					public void onAnimationEnd(Animation animation) {
+//						mContext.startActivity(new Intent(mContext,
+//								VoiceActivity.class));
+						Toast.makeText(MainActivity.this, "语音按钮监听", Toast.LENGTH_LONG).show();
+						closeUgc();
+					}
+				});
+				mUgcVoice.startAnimation(anim);
+			}
+		});
+		// Path 拍照按钮监听
+		mUgcPhoto.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				Animation anim = UgcAnimations.clickAnimation(500);
+				anim.setAnimationListener(new AnimationListener() {
+
+					public void onAnimationStart(Animation animation) {
+
+					}
+
+					public void onAnimationRepeat(Animation animation) {
+
+					}
+
+					public void onAnimationEnd(Animation animation) {
+						Toast.makeText(MainActivity.this, "拍照按钮监听", Toast.LENGTH_LONG).show();
+						
+						closeUgc();
+					}
+				});
+				mUgcPhoto.startAnimation(anim);
+			}
+		});
+		// Path 记录按钮监听
+		mUgcRecord.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				Animation anim = UgcAnimations.clickAnimation(500);
+				anim.setAnimationListener(new AnimationListener() {
+
+					public void onAnimationStart(Animation animation) {
+
+					}
+
+					public void onAnimationRepeat(Animation animation) {
+
+					}
+
+					public void onAnimationEnd(Animation animation) {
+						Toast.makeText(MainActivity.this, "记录按钮监听", Toast.LENGTH_LONG).show();
+						
+						closeUgc();
+					}
+				});
+				mUgcRecord.startAnimation(anim);
+			}
+		});
+		// Path 签到按钮监听
+		mUgcLbs.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				Animation anim = UgcAnimations.clickAnimation(500);
+				anim.setAnimationListener(new AnimationListener() {
+
+					public void onAnimationStart(Animation animation) {
+
+					}
+
+					public void onAnimationRepeat(Animation animation) {
+
+					}
+
+					public void onAnimationEnd(Animation animation) {
+						Toast.makeText(MainActivity.this, " 签到按钮监听", Toast.LENGTH_LONG).show();
+						
+						closeUgc();
+					}
+				});
+				mUgcLbs.startAnimation(anim);
+			}
+		});
+	}
+
+	
+	/**
+	 * 关闭Path菜单
+	 */
+	public void closeUgc() {
+		mUgcIsShowing = false;
+		UgcAnimations.startCloseAnimation(mUgcLayout, mUgcBg, mUgc, 500);
 	}
 
 }
